6231a1efa0cabc7b4508689336092a9e771137be
diasbuz <grandleocraft@gmail.com>
2025-07-18 20:37:15 +0600

    Added parser and command dispatcher1

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -4,20 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ClientHandler implements Runnable{
-    private Map<String, String> map = new HashMap<>();
-
     private Socket socket;
+    private BufferedWriter bufferedWriter;
 
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
+        try {
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
     @Override
     public void run() {
         CommandDispatcher commandDispatcher = new CommandDispatcher();
         try {
-            commandDispatcher.dispatch(RequestHandler.parseRespCommand(socket.getInputStream()));
+            bufferedWriter.write(commandDispatcher.dispatch(RequestHandler.parseRespCommand(socket.getInputStream())));
+            bufferedWriter.flush();
             socket.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
